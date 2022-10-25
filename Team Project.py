5fc51a4dc450365bfe27d8e91b0448a19bc815de
Bcarrian <115441006+Bcarrian@users.noreply.github.com>
2022-10-25 14:09:23 -0500

    Add files via upload

---
+++ Team Project.py
@@ -0,0 +1,30 @@
+print("Please choose your option from the list below:")
+print("1. Learn Java")
+print("2. Learn Python")
+print("3. Learn Soccer")
+print("4. Have dinner")
+print("5. Go to bed")
+print("0. Exit")
+
+Option = input("Choose option from 1-5:")
+if Option == '1':
+    print("You are going to enjoy learning Java")
+if Option == '2':
+    print("Python is a very fun programing language to learn!")
+if Option == '3':
+    print("Wow! You like soccer?, that seems like a fun sport!")
+if Option == '4':
+    print("We ate pasta today!")
+if Option == '5':
+    print("You seem tired? Go ahead and get some rest.")
+if Option == '0':
+    print("Goodbye")
+else:
+    print("Try another choice!")
+
+
+
+
+
+
+
